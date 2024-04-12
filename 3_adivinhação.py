7952fd084ac435d0049f8d143a760aca4dd2ac01
Anton-gaab77 <146292309+Anton-gaab77@users.noreply.github.com>
2024-04-11 20:56:15 -0400

    Create 3_adivinhação.py

---
+++ 3_adivinhação.py
@@ -0,0 +1,19 @@
+import random
+numero_secreto = random.randint(1, 10)
+tentativas = 3
+
+while tentativas > 0:
+    palpite = int(input("Adivinhe o número entre 1 e 10: "))
+    if palpite == numero_secreto:
+        print("Parabéns! Você acertou.")
+        break
+    elif palpite < numero_secreto:
+        print("Seu palpite está muito baixo.")
+    else:
+        print("Seu palpite está muito alto.")
+    tentativas -= 1
+
+if tentativas == 0:
+    print("Suas tentativas acabaram. O número secreto era:", numero_secreto)
+
+print("Fim do jogo.")
